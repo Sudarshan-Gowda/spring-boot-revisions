88454e6d17c595edae95750156dd75572fc5417f
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2025-02-22 20:39:02 +0530

    "tight coupling example using java"

---
+++ 01-coupling/src/main/java/com/star/sud/game/SuperContra.java
@@ -0,0 +1,20 @@
+package com.star.sud.game;
+
+public class SuperContra implements GamingConsole{
+
+    public void left() {
+        System.out.println("#Left running");
+    }
+
+    public void right() {
+        System.out.println("#Right running");
+    }
+
+    public void up() {
+        System.out.println("#Up running");
+    }
+
+    public void down() {
+        System.out.print("#Down running");
+    }
+}
